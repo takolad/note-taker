84b72b24621dbacd3f2913b5e2dc51c6d2ca5e4b
John Hinojosa <hinojohn@ymail.com>
2021-04-07 14:27:00 -0500

    added more (necessary?) routes to server.js, added uuid to project, manually added additional entry to db.json for testing

--- server.js
+++ server.js
@@ -1,28 +1,44 @@
 // Dependencies
 const express = require('express');
+const { v4: uuidv4 } = require('uuid');
 const path = require('path');
 const fs = require('fs');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Unsure if this is necessary //
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 // Routes
 
 // Basic routes
-app.get('*', (req, res) => res.sendFile(path.join(__dirname, './public/index.html')));
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public/index.html')));
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'public/notes.html')));
+app.get('/api/notes', (req,res) => res.json(path.join(__dirname, 'db/db.json')));
 
-app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, './public/notes.html')));
+// this worked, did I have to do this?
+app.get('/assets/css/styles.css', (req, res) => res.sendFile(path.join(__dirname, 'public/assets/css/styles.css')));
+app.get('/assets/js/index.js', (req, res) => res.sendFile(path.join(__dirname, 'public/assets/js/index.js')));
 
-app.get('/api/notes', (req,res) => res.json(path.join(__dirname, './db/db.json')));
 
 app.post('/api/notes', (req, res) => {
     const newNote = req.body;
 
-    newNote.routeName = newNote.title.replace(/\s+/g, '').toLowerCase();
+    newNote.id = uuidv4();
     console.log(newNote);
 
-    // write newNote to db.json
-    fs.appendFileSync(path.join(__dirname, './db/db.json'), JSON.stringify(newNote));
+    // read json file and set to array
+    fs.readFileSync(path.join(__dirname, './db/db.json'), 'utf-8', (err, data) => {
+        const noteArray = JSON.parse(data);
+    })
+    // push new entry into array
+    noteArray.push(newNote);
+
+    // write array back to json file
+    fs.writeFileSync(path.join(__dirname, './db/db.json'), JSON.stringify(noteArray));
     // res with newNote
     res.json(newNote);
 })
